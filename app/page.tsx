91d14c1df75d52f49d62cb0145eebbc6bf69c312
Monu_2114 <monu.nanabala@gmail.com>
2025-06-21 01:28:05 +0530

    header,layout,navlink

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Secondary</Button>
+    <div className="">
+      <h1>Samurai</h1>
+      <Button variant={"outline"}>Secondary</Button>
     </div>
   );
 }
