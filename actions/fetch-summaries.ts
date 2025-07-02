af237dd87d1a607ff042cc476a80cf0288feb729
Monu_2114 <monu.nanabala@gmail.com>
2025-07-02 21:03:38 +0530

    dynamic page routing (individual summary)

--- actions/fetch-summaries.ts
+++ actions/fetch-summaries.ts
@@ -6,3 +6,19 @@ export async function fetchSummaries({ user_id }: { user_id: string | null }) {
     await sql`SELECT id,user_id,original_file_url,status,summary_text,title,file_name,created_at FROM pdf_summaries WHERE user_id=${user_id}`;
   return summaries as SummaryInput[];
 }
+
+export default async function GetSummaryById(id: string) {
+  try {
+    const sql = await getDbConnection();
+    const [summary] =
+      await sql`SELECT id, user_id, title, original_file_url, summary_text, created_at, updated_at, status, file_name, 
+LENGTH(summary_text) - LENGTH(REPLACE(summary_text, ' ', '')) + 1 as word_count 
+FROM pdf_summaries 
+WHERE id = ${id} 
+ORDER BY created_at DESC;
+`;
+    return summary;
+  } catch (err) {
+    console.error("Error fetching summary by id", err);
+  }
+}
